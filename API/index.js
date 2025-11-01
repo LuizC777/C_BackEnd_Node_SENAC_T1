122335bb6a1caebb2a04a99ade161b30d1f1cbda
LUIZ CLARO LIMA <luiz.clima5@senacsp.edu.br>
2025-11-01 11:52:00 -0300

    poo e api

---
+++ API/index.js
@@ -0,0 +1,15 @@
+import express from "express"
+
+const app = express()
+
+
+// serviço = API
+
+// rota que o cliente irá solicitar
+app.get("/", (req,res) =>{
+    res.send("Primeiro dia com node")
+})
+
+app.listen(3000, ()=>{
+    console.log("Iniciando minha API com express")
+})
